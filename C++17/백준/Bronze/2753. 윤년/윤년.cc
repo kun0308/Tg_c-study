38987395d727d0f16e2b28b3f9525c8068a9b666
kun0308 <117059689+kun0308@users.noreply.github.com>
2026-03-24 13:57:52 +0900

    [Bronze V] Title: 윤년, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/2753. 윤년/윤년.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void){
+    int y;
+    cin >> y;
+    if (y % 400 == 0){
+        cout << '1';
+    }
+    else if (y%4 == 0 && y% 100 != 0) {
+        cout << '1';
+        
+    }
+    else cout << '0';
+    return 0;
+}
